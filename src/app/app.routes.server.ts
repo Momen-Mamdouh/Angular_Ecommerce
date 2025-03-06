ab9bbfa1c60fe634d52ac6dae0ab165d54743c1b
Mo'men Mamdouh <momenmamdouhw@gmail.com>
2025-03-06 09:56:50 +0200

    LastCommiting

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,21 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'shop/:shopCategory',
+    renderMode: RenderMode.Client
+  },
+
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Client
+  },
+
+  {
+    path: 'checkOut/:cartId',
+    renderMode: RenderMode.Client
+  },
+
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
